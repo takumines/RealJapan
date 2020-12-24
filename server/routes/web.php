e17c90dc71c2c9ce791adb9a9d019e3ca6575db7
takumi_kurogi <takumines1@gmail.com>
2020-12-24 18:23:50 +0900

    [update]ルーティング修正

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\RealEstateSearchController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [RealEstateSearchController::class, 'index']);
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
