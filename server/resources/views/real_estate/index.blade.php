5e73c24c314fd58db7160ce6e76441761b5f5e5a
takumi_kurogi <takumines1@gmail.com>
2021-01-07 19:32:00 +0900

    [update]検索フォームのレイアウトを整えた

--- server/resources/views/real_estate/index.blade.php
+++ server/resources/views/real_estate/index.blade.php
@@ -4,31 +4,67 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+                <h1 class="mt-3">Real Japan</h1>
+                <div class="card mt-5">
                     <div class="card-header">
                         不動産取引検索
                     </div>
                     <div class="card-body">
-                        <form class="form-horizontal" method="get" action="#"
+                        <form class="form-horizontal" method="post" action="#"
                               enctype="multipart/form-data">
                             <div class="form-group row">
-                                <label class="col-sm-6 col-form-label text-md-right">都道府県</label>
-                                <div class="col-sm-6 col-form-label">
-                                    <select name="prefecture" class="form-control prefecture" >
-                                        <option value="">選択してください</option>
-                                        @foreach( $prefectures as $key => $value )
-                                            <option
-                                                    value="{{ $value }}">
-                                                {{ $key }}
-                                            </option>
-                                        @endforeach
-                                    </select>
+                                <div class="col-4">
+                                    <label class="col-form-label">都道府県</label>
+                                    <div class="col-form-label">
+                                        <select name="prefecture" class="form-control prefecture" >
+                                            <option value="">選択してください</option>
+                                            @foreach( $prefectures as $key => $value )
+                                                <option
+                                                        value="{{ $value }}">
+                                                    {{ $key }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </div>
                                 </div>
-                                <label class="col-sm-6 col-form-label text-md-right">市区町</label>
-                                <div class="col-sm-6 col-form-label">
-                                    <select name="city" class="form-control city" >
-                                        <option value="">選択してください</option>
-                                    </select>
+                                <div class="col-4">
+                                    <label class="col-form-label">市区町</label>
+                                    <div class="col-form-label">
+                                        <select name="city" class="form-control city" >
+                                            <option>選択してください</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <label class="col-form-label">地域</label>
+                                    <div class="col-form-label">
+                                        <select name="area" class="form-control area">
+                                            <option>選択してください</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <div class="col-4">
+                                    <label class="col-form-label">物件タイプ</label>
+                                    <div class="col-form-label">
+                                        <select name="property_type" class="form-control property_type">
+                                            <option>選択してください</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <label class="col-form-label">価格</label>
+                                    <div class="col-form-label">
+                                        <input type="text" name="price" value="{{ old('price') }}" class="form-control" placeholder="入力してください">
+                                    </div>
+                                </div>
+                                <div class="col-4">
+                                    <label class="col-form-label">面積</label>
+                                    <div class="col-form-label">
+                                        <input type="text" name="square_meters" value="{{ old('square_meters') }}" class="form-control" placeholder="入力してください">
+                                    </div>
                                 </div>
                             </div>
 
